7d56fc1e6735a5d744b8c3358caf028bf4bf873c
Maksuda Akter <maksudaakterliza13@gmail.com>
2026-09-17 20:27:05 +0600

    For Loop

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,8 @@
 // ================================== For Loop ==============================
-for ( let i=0; i<=10; i++) {
+for ( let i=1; i<=10; i++) {
     console.log(`8 * ${i} = ${8*i}`)
+}
+let namta = 10
+for ( let i=1; i<=10; i++) {
+    console.log(`${namta} * ${i} = ${namta*i}`)
 }
