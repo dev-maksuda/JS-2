35f1a9ae41c3f519ab04971ef4998b3f1e9e4377
Maksuda Akter <maksudaakterliza13@gmail.com>
2026-09-17 21:11:03 +0600

    while & Do while

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,26 @@
 for ( let i=1; i<=10; i++) {
     console.log(`8 * ${i} = ${8*i}`)
 }
+
 let namta = 10
 for ( let i=1; i<=10; i++) {
     console.log(`${namta} * ${i} = ${namta*i}`)
 }
+
+for ( let i=1; i<=10; i++) {
+    console.log(`Happy ${i}`)
+}
+// ===================================== While Loop =============================
+let a=1
+while (a <=10) {
+  console.log(a)
+  a++;
+}
+// ==================================== Do While Loop ============================
+let b=1
+do {
+ console.log(b);
+  b++;
+}
+while (b<=10);
+
