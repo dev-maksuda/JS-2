bee120e7bca58c29f1f31327d336f312974c55c2
Maksuda Akter <maksudaakterliza13@gmail.com>
2026-09-18 19:55:18 +0600

    function declaration

--- JS/Function.js
+++ JS/Function.js
@@ -1,5 +1,28 @@
-function sayHello() {
-  return "Hello World";
-}
+// ============================= Function declaration ===============================
 
-let message = sayHello();
+// function jog() {
+// console.log ("Hello World");
+// }
+// jog()
+// jog()
+// jog()
+// jog()
+// jog()
+
+// function jog(a,b) {
+// console.log (a+b);
+// }
+// jog(10, 20)
+// jog(100, 20)
+// jog(150, 50)
+// jog(170, 30)
+
+// function jog(a = 0,b = 0) {
+// console.log (a + b);
+//  }
+//  jog(30, 20)
+//  jog(130, 20)
+//  jog(310)
+
+
+// ============================= Function Expression ===============================
